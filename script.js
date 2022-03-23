293582ef4dd83c7b6b57c4f852a88f513f882692
andrewske <andrewskevin92@gmail.com>
2022-03-23 16:37:12 -0700

    remove console logs

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const grid = new Grid(gameBoard);
 
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
-console.log(grid.cellsByColumn);
 setupInput();
 
 function setupInput() {
